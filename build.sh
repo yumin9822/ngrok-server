e2e8778cb9e10a9833dae4bb299e422912fa6cef
yumin9822 <yumin9822@gmail.com>
2015-09-07 14:42:24 +0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+if [ "${CA_CERT}" == "**None**" ]; then
+    echo "Please specify CA_CERT"
+    exit 1
+fi
+
+
+echo -e "${CA_CERT}" > /ngrok/assets/client/tls/ngrokroot.crt
+
+if [ ! -f /ngrok/bin/ngrokd ]; then
+    echo "=> Compiling ngrok binary files"
+    cd /ngrok; make release-server release-client
+    echo "=> Successfully built the binaries"
+fi
